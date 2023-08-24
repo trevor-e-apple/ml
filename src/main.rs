5011dea53886b8157be40d4388e9fc8d81736e1e
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2023-08-23 20:14:25 -0700

    got a super basic plotters example working

--- src/main.rs
+++ src/main.rs
@@ -2,6 +2,8 @@ mod lib;
 
 use std::time::{SystemTime, UNIX_EPOCH};
 
+use plotters::prelude::{BitMapBackend, RED, DrawingBackend};
+
 use lib::Rng;
 
 fn main() {
@@ -12,4 +14,11 @@ fn main() {
         let random = rng.rand();
         println!("{random}");
     }
+
+    // Create a 800*600 bitmap and start drawing
+    let mut backend = BitMapBackend::new("test/1.png", (300, 200));
+    // And if we want SVG backend
+    // let backend = SVGBackend::new("output.svg", (800, 600));
+    backend.draw_rect((50,50), (200, 150), &RED, true).unwrap();
+    backend.present().unwrap();
 }
