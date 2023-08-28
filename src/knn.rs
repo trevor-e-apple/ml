5b42f16d5fd3aef7627b8b192896909cd7926fe8
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2023-08-27 22:49:24 -0700

    some light progress on knn

---
+++ src/knn.rs
@@ -0,0 +1,79 @@
+use std::collections::HashMap;
+
+type Class = i64;
+
+pub struct Knn {
+    k: usize,
+    pub data: Vec<(Class, Vec<f64>)>
+}
+
+impl Knn {
+    pub fn new(k: usize, data: Vec<(Class, Vec<f64>)>) -> Self {
+        Self {k, data}
+    }
+
+    // TODO: make this replaceable somehow (maybe with generics?)
+    fn calc_distance(a: &[f64], b: &[f64]) -> f64 {
+        let mut distance = 0.0;
+        for (a_item, b_item) in a.iter().zip(b) {
+            distance += (a_item - b_item).powf(2.0);
+        }
+
+        distance.sqrt()
+    }
+
+    pub fn predict_class(&self, point: Vec<f64>) -> Class {
+        struct NeighborData {
+            distance: f64,
+            class: i64,
+        }
+
+        // initialize with first k neighbors
+        let mut nearest_neighbors: Vec<NeighborData> = Vec::with_capacity(self.k);
+        for datum in &self.data[0..self.k] {
+            let class = datum.0;
+            let distance = Self::calc_distance(&point, &datum.1);
+            nearest_neighbors.push(NeighborData { distance, class });
+        }
+
+        // find the k nearest neighbors
+        for datum in &self.data[self.k..] {
+
+        }
+
+        // map the classes to the neighbor count
+        let mut class_map: HashMap<Class, i32> = HashMap::with_capacity(self.k);
+        for neighbor_data in nearest_neighbors {
+            let count = match class_map.get(&neighbor_data.class) {
+                Some(count) => *count + 1,
+                None => 1,
+            };
+
+            class_map.insert(neighbor_data.class, count);
+        }
+
+        // find the class with the most close neighbors
+        let class = {
+            let mut class_map_iter = class_map.iter();
+            let first_pair = match class_map_iter.next() {
+                Some(pair) => pair,
+                None => panic!(),
+            };
+
+            let mut nearest_neighbor_class = first_pair.0;
+            let mut nearest_neighbor_count = first_pair.1;
+            for (neighbor_class, neighbor_count) in class_map_iter {
+                if neighbor_count > nearest_neighbor_count {
+                    nearest_neighbor_class = neighbor_class;
+                    nearest_neighbor_count = neighbor_count;
+                }
+            }
+
+            *nearest_neighbor_class
+        };
+
+        class
+    }
+}
+
+// todo: some testing
---
+++ src/rng.rs
@@ -0,0 +1,36 @@
+use std::f64::consts::PI;
+
+pub fn add(left: usize, right: usize) -> usize {
+    left + right
+}
+
+/// quick and dirty RNG
+pub struct Rng {
+    state: u64,
+}
+
+impl Rng {
+    /// seed must be non-zero
+    pub fn new(seed: u64) -> Self {
+        Self { state: seed }
+    }
+
+    pub fn rand(&mut self) -> u64 {
+        self.state ^= self.state << 13;
+        self.state ^= self.state >> 7;
+        self.state ^= self.state << 17;
+
+        self.state
+    }
+}
+
+/// function for sampling from a normal distribution using a random number
+/// generator that generates a uniform distribution
+pub fn box_muller(rng: &mut  Rng, mean: f64, std_dev: f64) -> f64 {
+    let u1 = (rng.rand() as f64) / (std::u64::MAX as f64);
+    let u2 = (rng.rand() as f64) / (std::u64::MAX as f64);
+
+    let z1 = (-2.0 * u1.ln()).sqrt() * (2.0 * PI * u2).cos();
+
+    z1 * std_dev + mean
+}
