8bd42e85a85d9f628a627cddcc7613ef35e80a9e
Trevor Apple <trevorapple@Trevors-MacBook-Air.local>
2023-11-07 08:12:51 -0800

    more temp work on metal

--- src/main.rs
+++ src/main.rs
@@ -89,12 +89,12 @@ fn gen_xor_data(
     (data, labels)
 }
 
-fn gen_random_data_mtl_buff(buff: &Buffer) {
-    let pointer: *mut i32 = buff.contents() as *mut i32;
+fn gen_data_mtl_buff(buff: &Buffer) {
+    let pointer: *mut f32 = buff.contents() as *mut f32;
     let length = buff.length() as usize;
     let contents = unsafe { slice::from_raw_parts_mut(pointer, length) };
     for index in 0..length {
-        contents[index] = 0;
+        contents[index] = index as f32;
     }
 }
 
@@ -174,8 +174,6 @@ fn main() {
 
     // metal test
     {
-        let device = Device::system_default().unwrap();
-
         let library_path = path::PathBuf::from(env!("CARGO_MANIFEST_DIR")).join("src/metal_kernels/add_array.metal");
         println!("{}", library_path.as_path().display().to_string());
 
@@ -188,10 +186,31 @@ fn main() {
         let options = CompileOptions::new();
         let _library = device.new_library_with_source(&contents, &options).unwrap();
         let function = _library.get_function("add_arrays", None).unwrap();
-        let pipeline = device.new_compute_pipeline_state_with_function(&function);
+        let pipeline = device.new_compute_pipeline_state_with_function(&function).unwrap();
         let command_queue = device.new_command_queue();
-        let buffer_one = device.new_buffer(8192, MTLResourceOptions::empty());
-        let buffer_two = device.new_buffer(8192, MTLResourceOptions::empty());
-        let buffer_three = device.new_buffer(8192, MTLResourceOptions::empty());
+        let array_length: u64 = 8192;
+        let mut buffer_one = device.new_buffer(array_length, MTLResourceOptions::empty());
+        let mut buffer_two = device.new_buffer(array_length, MTLResourceOptions::empty());
+        let mut buffer_three = device.new_buffer(array_length, MTLResourceOptions::empty());
+
+        gen_data_mtl_buff(&mut buffer_one);
+        gen_data_mtl_buff(&mut buffer_two);
+        gen_data_mtl_buff(&mut buffer_three);
+
+        let command_buffer = command_queue.new_command_buffer();
+        let compute_encoder = command_buffer.new_compute_command_encoder();
+
+        compute_encoder.set_compute_pipeline_state(&pipeline);
+        compute_encoder.set_buffer(0, Some(&buffer_one), 0);
+        compute_encoder.set_buffer(0, Some(&buffer_two), 1);
+        compute_encoder.set_buffer(0, Some(&buffer_three), 2);
+        let grid_size = MTLSize { width: array_length, height: 1, depth: 1 };
+        let thread_group_len = pipeline.max_total_threads_per_threadgroup();
+        let thread_group_len = if thread_group_len > array_length {
+            array_length
+        } else {
+            thread_group_len
+        };
+        let thread_group_size = MTLSize { width: thread_group_len, height: 1, depth: 1 };
     }
 }
